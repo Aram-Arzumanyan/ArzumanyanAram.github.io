1b6f0fe426906ad0e684a6adb83f3812acbbae9e
Aram Arzumanyan <aram.arz@mail.ru>
2023-03-07 05:52:45 +1030

    nev

--- src/assets/components/img_block/index.jsx
+++ src/assets/components/img_block/index.jsx
@@ -8,7 +8,7 @@ function Img_block(props) {
         className="imag"
         style={{ backgroundImage: `url('${item.img}')` }}
       ></div>
-      <div style={{width:'75%',lineHeight:'1.5'}} >
+      <div className="textimg" >
         <h4 style={{ color: "rgb(26 102 146)" }}>{item.name}</h4>
         <p style={{ color: "#626364" }}>{item.info}</p>
         <p style={{ color: "#2f956d" }}>{item.value}</p>
